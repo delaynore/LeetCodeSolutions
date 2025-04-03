401142ba524e2723cca959bdb1e0d3ff7eb56e13
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 18:00:19 +0400

    problem 283

---
+++ LeetCodeSolutions/TwoPointers/MoveZeroes_283.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class MoveZeroes_283
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var x = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                (nums[i], nums[x]) = (nums[x], nums[i]);
+                x++;
+            }
+        }
+    }
+}
