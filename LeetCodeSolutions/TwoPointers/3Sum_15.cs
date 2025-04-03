a866de7aa937d8a978c83493b7f3805ec11f4f50
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 17:53:48 +0400

    problem 15

---
+++ LeetCodeSolutions/TwoPointers/3Sum_15.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class _3Sum_15
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var list = new HashSet<(int X, int Y, int Z)>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var target = -nums[i];
+            var left = i + 1;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var currentSum = nums[left] + nums[right];
+                if (currentSum == target)
+                {
+                    list.Add((nums[i], nums[left], nums[right]));
+                }
+
+                if (currentSum > target)
+                {
+                    right--;
+                }
+                else left++;
+
+            }
+        }
+
+        return [.. list.Select(IList<int> (x) => [x.X, x.Y, x.Z])];
+    }
+}
