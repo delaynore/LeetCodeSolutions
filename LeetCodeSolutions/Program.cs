d511082f5eb884550cb8d4dafe90f168f25f22a6
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 19:13:35 +0400

    209

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LeetCodeSolutions.SlidingWindow;
+
+new MinimumSizeSubarraySum_209.Solution().MinSubArrayLen(7, [2, 3, 1, 2, 4, 3]);
---
+++ LeetCodeSolutions/SlidingWindow/MinimumSizeSubarraySum_209.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutions.SlidingWindow;
+
+internal class MinimumSizeSubarraySum_209
+{
+    public class Solution
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            var begin = 0;
+            var windowSize = int.MaxValue;
+            var windowSum = 0;
+            for (var end = 0; end < nums.Length; end++)
+            {
+                windowSum += nums[end];
+
+                while (windowSum >= target)
+                {
+                    windowSize = Math.Min(windowSize, end - begin + 1);
+                    windowSum -= nums[begin];
+                    begin++;
+                }
+            }
+
+            return windowSize == int.MaxValue ? 0 : windowSize;
+        }
+    }
+}
