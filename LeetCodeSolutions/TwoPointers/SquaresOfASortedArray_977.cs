21b5e73f3e9aabe3d75b80ebf281cd7c39f61533
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 17:57:32 +0400

    problem 977

---
+++ LeetCodeSolutions/TwoPointers/SquaresOfASortedArray_977.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class SquaresOfASortedArray_977
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var left = 0;
+        var right = nums.Length - 1;
+        var current = right;
+
+        while (left < right)
+        {
+            var leftS = nums[left] * nums[left];
+            var rightS = nums[right] * nums[right];
+
+            if (leftS > rightS)
+            {
+                result[current] = leftS;
+                left++;
+            }
+            else
+            {
+                result[current] = rightS;
+                right--;
+            }
+            current--;
+        }
+
+        // after cycle one square left.
+        // and cause right == left, choose any
+        result[current] = nums[right] * nums[right];
+
+        return result;
+    }
+}
