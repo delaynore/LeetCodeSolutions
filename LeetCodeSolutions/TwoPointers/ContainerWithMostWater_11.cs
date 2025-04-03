8a88668f31fe6c9db99b175a3bfee0c375e1c3ee
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 17:58:50 +0400

    problem 11

---
+++ LeetCodeSolutions/TwoPointers/ContainerWithMostWater_11.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class ContainerWithMostWater_11
+{
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var maxArea = int.MinValue;
+
+        while (left < right)
+        {
+            var v = (right - left) * Math.Min(height[left], height[right]);
+            maxArea = Math.Max(v, maxArea);
+
+            if (height[left] > height[right])
+            {
+                right--;
+            }
+            else left++;
+        }
+
+        return maxArea;
+    }
+}
