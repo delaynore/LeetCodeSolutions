a5580563d585d629d11ebddc56877803dbc291e3
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 17:46:24 +0400

    problem 344

---
+++ LeetCodeSolutions/TwoPointers/ReverseString_344.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class ReverseString_344
+{
+    public void ReverseString(char[] s)
+    {
+        var l = 0;
+        var r = s.Length - 1;
+        while (l < r)
+        {
+            (s[l], s[r]) = (s[r], s[l]);
+            l++;
+            r--;
+        }
+    }
+}
