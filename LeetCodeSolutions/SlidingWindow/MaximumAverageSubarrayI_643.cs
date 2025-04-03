3a1ef4492130337f4a6680efa96a9dfe231c5435
Daniil Lozhkin <delaynore@gmail.com>
2025-04-03 18:06:29 +0400

    643

---
+++ LeetCodeSolutions/SlidingWindow/MaximumAverageSubarrayI_643.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.SlidingWindow;
+
+internal class MaximumAverageSubarrayI_643
+{
+    public class Solution
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            var max = int.MinValue;
+            var sum = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (i - k >= 0)
+                {
+                    sum -= nums[i - k];
+                }
+                if (i >= k - 1) max = Math.Max(sum, max);
+            }
+
+            return max / (double)k;
+        }
+    }
+}
