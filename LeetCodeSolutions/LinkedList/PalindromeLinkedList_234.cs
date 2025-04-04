088218ecb0dcf3a1833e239085145688e1dfa5ef
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 16:18:58 +0400

    234

---
+++ LeetCodeSolutions/LinkedList/PalindromeLinkedList_234.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class PalindromeLinkedList_234
+{
+    public class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            var middle = MiddleNode(head);
+            var second = ReverseList(middle);
+            var first = head;
+
+            while (first != null && second != null)
+            {
+                if (first.val != second.val) return false;
+                first = first.next;
+                second = second.next;
+            }
+            return true;
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            var current = head;
+            ListNode? prev = null;
+            while (current != null)
+            {
+                var tmp = current.next;
+                current.next = prev!;
+                prev = current;
+                current = tmp;
+            }
+
+            return prev!;
+        }
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            var middle = head;
+            var end = head;
+            while (end?.next != null)
+            {
+                end = end.next?.next;
+                middle = middle.next;
+            }
+
+            return middle;
+        }
+    }
+}
