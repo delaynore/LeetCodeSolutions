5ac891765f9f313338f162c3d56c68a00f20dcee
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 14:45:18 +0400

    2095

---
+++ LeetCodeSolutions/LinkedList/DeleteTheMiddleNodeOfALinkedList_2095.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class DeleteTheMiddleNodeOfALinkedList_2095
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null!)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null) return null!;
+
+            var middle = head;
+            var end = head.next?.next;
+            while (end?.next != null)
+            {
+                end = end.next?.next;
+                middle = middle.next;
+            }
+
+            middle.next = middle.next.next;
+
+            return head;
+        }
+    }
+}
