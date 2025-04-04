74bee82645e75f2cf1051d4fd45f8d82e29be554
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 11:43:38 +0400

    904

---
+++ LeetCodeSolutions/SlidingWindow/FruitIntoBasket_904.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeSolutions.SlidingWindow;
+
+internal class FruitIntoBasket_904
+{
+    public class Solution
+    {
+        public int TotalFruit(int[] fruits)
+        {
+            const int Size = 2;
+            var dict = new Dictionary<int, int>();
+            var maxWindowSize = 0;
+            var begin = 0;
+            for (var end = 0; end < fruits.Length; end++)
+            {
+                dict[fruits[end]] = dict.TryGetValue(fruits[end], out var value)
+                    ? value + 1
+                    : 1;
+
+                while (dict.Count > Size)
+                {
+                    dict[fruits[begin]]--;
+
+                    if (dict.TryGetValue(fruits[begin], out var value1) && value1 == 0)
+                    {
+                        dict.Remove(fruits[begin]);
+                    }
+                    begin++;
+                }
+
+                maxWindowSize = Math.Max(maxWindowSize, end - begin + 1);
+            }
+
+            return maxWindowSize;
+        }
+    }
+}
