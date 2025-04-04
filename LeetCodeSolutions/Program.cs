5408134767bcc9d5442322d3b1f077122ec827e4
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 11:23:27 +0400

    1493

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCodeSolutions.SlidingWindow;
 
-new MaxConsecutiveOnesIII_1004.Solution().LongestOnes([0, 0, 0,1], 4);
+new LongestSubarrayOf1AfterDeletingOneElement_1493.Solution().LongestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]);
---
+++ LeetCodeSolutions/SlidingWindow/LongestSubarrayOf1AfterDeletingOneElement_1493.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolutions.SlidingWindow;
+
+internal class LongestSubarrayOf1AfterDeletingOneElement_1493
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            const int k = 1;
+            var begin = 0;
+            var maxWindowSize = int.MinValue;
+            var zeroCounter = 0;
+            for (var end = 0; end < nums.Length; end++)
+            {
+                if (nums[end] == 0)
+                {
+                    zeroCounter++;
+                }
+                while (zeroCounter > k)
+                {
+                    if (nums[begin] == 0)
+                    {
+                        zeroCounter--;
+                    }
+                    begin++;
+                }
+                maxWindowSize = Math.Max(maxWindowSize, end - begin); // must delete one element, even if there is no 0
+            }
+            return maxWindowSize;
+        }
+    }
+}
