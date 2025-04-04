d79b96fc27efd6b355d07ccf5ac6c5c391b768aa
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 16:39:15 +0400

    83

--- LeetCodeSolutions/LinkedList/ListNode.cs
+++ LeetCodeSolutions/LinkedList/ListNode.cs
@@ -10,4 +10,11 @@ public class ListNode
         this.val = val;
         this.next = next;
     }
+
+    public override string ToString()
+    {
+        if(next == null) return val.ToString();
+
+        return $"{val} -> {next}";
+    }
 }
---
+++ LeetCodeSolutions/LinkedList/RemoveDuplicatesFromSortedList_83.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class RemoveDuplicatesFromSortedList_83
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var current = head;
+            while (current?.next != null)
+            {
+                if (current.next.val == current.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+    }
+}
