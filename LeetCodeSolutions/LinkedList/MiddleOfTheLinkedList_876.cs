d5cd9df15149745cf0ed4dd805d6f28feacb0127
Daniil Lozhkin <delaynore@gmail.com>
2025-04-04 15:33:29 +0400

    cleanup

--- LeetCodeSolutions/LinkedList/MiddleOfTheLinkedList_876.cs
+++ LeetCodeSolutions/LinkedList/MiddleOfTheLinkedList_876.cs
@@ -2,19 +2,6 @@
 
 internal class MiddleOfTheLinkedList_876
 {
-
-    //Definition for singly-linked list.
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public class Solution
     {
         public ListNode MiddleNode(ListNode head)
---
+++ LeetCodeSolutions/LinkedList/ReverseLinkedListII_92.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class ReverseLinkedListII_92
+{
+    public class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            left--;
+            right--;
+
+            var current = head;
+            var count = 0;
+            while (count < left - 1)
+            {
+                current = current.next;
+            }
+            var prev = current;
+            current = current.next;
+
+            while (count < right)
+            {
+                var tmp = current.next;
+                current.next = prev;
+                prev = current;
+                current = tmp;
+                count++;
+            }
+
+            return head;
+        }
+    }
+}
