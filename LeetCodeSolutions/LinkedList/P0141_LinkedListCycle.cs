2fe188f23a5b419bdfebd71d14aaae551bd41d8c
Daniil Lozhkin <delaynore@gmail.com>
2025-04-08 00:23:09 +0400

    141

---
+++ LeetCodeSolutions/LinkedList/P0141_LinkedListCycle.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class P0141_LinkedListCycle
+{
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (fast?.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
