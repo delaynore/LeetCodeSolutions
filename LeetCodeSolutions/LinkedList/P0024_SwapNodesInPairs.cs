6b3609e434229ed81968822635e10cb1fc469199
Daniil Lozhkin <delaynore@gmail.com>
2025-04-07 23:46:17 +0400

    24

---
+++ LeetCodeSolutions/LinkedList/P0024_SwapNodesInPairs.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutions.LinkedList;
+
+internal class P0024_SwapNodesInPairs
+{
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            var dummy = new ListNode(0, head);
+
+            var current = dummy;
+            while (current?.next?.next != null)
+            {
+                var first = current.next;
+                var second = current.next.next;
+
+                first.next = second.next;
+                second.next = first;
+                current.next = second;
+
+                current = first;
+            }
+
+            return dummy.next;
+        }
+    }
+}
