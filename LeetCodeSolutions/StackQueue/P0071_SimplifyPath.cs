bab094293af614303ddfe1650dee25074a3222e7
Daniil Lozhkin <delaynore@gmail.com>
2025-04-14 23:35:43 +0400

    71

---
+++ LeetCodeSolutions/StackQueue/P0071_SimplifyPath.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LeetCodeSolutions.StackQueue;
+
+internal class P0071_SimplifyPath
+{
+    public class Solution
+    {
+        public string SimplifyPath(string path)
+        {
+            var p = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            var stack = new LinkedList<string>();
+            foreach (var dir in p)
+            {
+                if (dir == ".") continue;
+                if (dir == "..")
+                {
+                    if (stack.Count > 0)
+                        stack.RemoveLast();
+                    continue;
+                }
+                stack.AddLast(dir);
+            }
+
+            var builder = new StringBuilder();
+            return
+                builder
+                    .Append('/')
+                    .AppendJoin('/', stack)
+                    .ToString();
+        }
+    }
+}
