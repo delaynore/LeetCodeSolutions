8ca0dee36749aff023381c385acf7e77a072f2ee
Daniil Lozhkin <delaynore@gmail.com>
2025-04-14 23:32:09 +0400

    20

---
+++ LeetCodeSolutions/StackQueue/P0020_ValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeSolutions.StackQueue;
+
+internal class P0020_ValidParentheses
+{
+    public class Solution
+    {
+        private static readonly Dictionary<char, char> _dictionary = new()
+        {
+            {'}', '{'},
+            {')', '('},
+            {']', '['},
+        };
+
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            stack.Push(s[0]);
+
+            foreach (var c in s.Skip(1))
+            {
+                if (!_dictionary.TryGetValue(c, out var closeParen))
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                if (stack.TryPeek(out var peek) && peek == closeParen)
+                {
+                    stack.Pop();
+                    continue;
+                }
+
+                return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
