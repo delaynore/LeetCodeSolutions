f90b62c47405e0d328fba4a5384c21afc0220d86
Daniil Lozhkin <delaynore@gmail.com>
2025-04-14 23:37:01 +0400

    933

---
+++ LeetCodeSolutions/StackQueue/P0933_NumberOfRecentCalls.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions.StackQueue;
+
+internal class P0933_NumberOfRecentCalls
+{
+    public class RecentCounter
+    {
+        private readonly LinkedList<int> _q = new();
+        private const int Window = 3000;
+
+        public int Ping(int t)
+        {
+            _q.AddLast(t);
+            var threshold = t - Window;
+
+            while (_q.First?.Value < threshold)
+            {
+                _q.RemoveFirst();
+            }
+
+            return _q.Count;
+        }
+    }
+}
