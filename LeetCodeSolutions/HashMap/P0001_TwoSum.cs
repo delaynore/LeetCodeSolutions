37ef5d10185480ff64fcb302dda7de76fa45ce1c
Daniil Lozhkin <delaynore@gmail.com>
2025-04-14 23:27:48 +0400

    1

---
+++ LeetCodeSolutions/HashMap/P0001_TwoSum.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeSolutions.HashMap;
+internal class P0001_TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var (index, value) in nums.Index())
+            {
+                if (dict.TryGetValue(target - value, out var i))
+                {
+                    return [index, i];
+                }
+                dict.TryAdd(value, index);
+            }
+
+            return [];
+        }
+    }
+}
