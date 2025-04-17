11da322c794af9352ead8370823c81b9edf25d7d
Daniil Lozhkin <delaynore@gmail.com>
2025-04-17 12:32:55 +0400

    72

--- LeetCodeSolutions/DynamicProgramming/P0072_EditDistance.cs
+++ LeetCodeSolutions/DynamicProgramming/P0072_EditDistance.cs
@@ -4,6 +4,7 @@ internal class P0072_EditDistance
 {
     public class Solution
     {
+        // Algo O(n*m), Space O(n*m)
         public int MinDistance(string word1, string word2)
         {
             var dp = new int[word2.Length + 1, word1.Length + 1];
@@ -24,6 +25,7 @@ internal class P0072_EditDistance
                     if(word1[p1 - 1] == word2[p2 - 1])
                     {
                         dp[p2, p1] = dp[p2-1, p1 - 1];
+                        continue;
                     }
 
                     dp[p2, p1] = 1 + Math.Min(
@@ -33,6 +35,40 @@ internal class P0072_EditDistance
             }
 
             return dp[word2.Length, word1.Length];
+        }
+
+
+        // Algo O(n*m), Space O(n)
+        public int MinDistanceShorted(string word1, string word2)
+        {
+            var prev = new int[word1.Length + 1];
+            var current = new int[word1.Length + 1];
+
+            for (var i = 1; i < word1.Length + 1; i++)
+            {
+                prev[i] = i;
+            }
+
+            for (var p2 = 1; p2 < word2.Length + 1; p2++)
+            {
+                current[0] = p2;
+                for (var p1 = 1; p1 < word1.Length + 1; p1++)
+                {
+                    if (word1[p1 - 1] == word2[p2 - 1])
+                    {
+                        current[p1] = prev[p1 - 1];
+                        continue;
+                    }
+
+                    current[p1] = 1 + Math.Min(
+                        prev[p1 - 1],
+                        Math.Min(prev[p1], current[p1 - 1]));
+                }
+
+                (current, prev) = (prev, current);
+            }
+
+            return prev[word1.Length];
         }
     }
 }
