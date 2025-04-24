5b1260a38c51a33d36bdc6c17fc0a5d6c47e97be
Daniil Lozhkin <delaynore@gmail.com>
2025-04-24 15:15:54 +0400

    485

---
+++ LeetCodeSolutions/TwoPointers/P0485_MaxConsecutiveOnes.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions.TwoPointers;
+
+internal class P0485_MaxConsecutiveOnes
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var zero = 0;
+            var max = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    max = Math.Max(max, i - zero);
+                    zero = i + 1;
+                }
+            }
+
+            max = Math.Max(max, nums.Length - zero);
+
+            return max;
+        }
+    }
+}
